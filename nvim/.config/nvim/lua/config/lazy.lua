ffa3fa82f7d587fbbf72cf03298001634bf4574d
Guilherme Teodoro <guilherme.m.teodoro@gmail.com>
2023-08-16 14:36:53 -0300

    add eslint extras

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "plugins" },
   },
   defaults = {

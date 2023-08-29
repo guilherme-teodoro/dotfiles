09ff601e310895bc56d111800eae1773dea0b4ec
Guilherme Teodoro <guilherme.m.teodoro@gmail.com>
2023-08-29 11:06:24 -0300

    change telescope to dropdown\

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,13 +2,32 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
+      defaults = {
+        layout_config = {
+          vertical = { width = 0.8 },
+        },
+      },
       pickers = {
+        git_files = {
+          theme = "dropdown",
+          layout_config = {
+            width = 0.8,
+          },
+        },
         live_grep = {
+          theme = "dropdown",
+          layout_config = {
+            width = 0.8,
+          },
           additional_args = function()
             return { "--hidden" }
           end,
         },
         find_files = {
+          layout_config = {
+            width = 0.8,
+          },
+          theme = "dropdown",
           additional_args = function()
             return { "--hidden" }
           end,
